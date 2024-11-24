048c54718b4c50dec0dfa50966cdcc0010b3934e
Sebastian Hutter <me@sebastian-hutter.dev>
2024-11-24 16:08:10 +0100

    use do blocks to render defined spacers etc

---
+++ build/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+// constants for the plugins,
+// e.g. semi hardcoded values to be used with the fsrg website
+
+define("FSRG_RUNDGANG_TERMIN_GROUP_FIELD", "rundgang_termin");
+define("FSRG_RUNDGANG_TERMIN_DATE_AND_TIME_FIELD", "datum_und_zeit");
+define("FSRG_RUNDGANG_TERMIN_TICKET_LINK_FIELD", "ticket_link");
+
+// predefined wp block fields, defined as templates on the website
+define("FSRG_BLOCK_SPACER_ALLE_25_REF", "<!-- wp:block {\"ref\":30124} /-->");
+define("FSRG_BLOCK_SPACER_ALLE_10_REF", "<!-- wp:block {\"ref\":30132} /-->");
+
+?>
--- build/list-tour-dates-of-season/render.php
+++ build/list-tour-dates-of-season/render.php
@@ -6,6 +6,9 @@
 
 
 <?php
+
+include_once(plugin_dir_path(__FILE__) . "/../constants.php");
+
 // get attributes from block
 $seasonYear = $attributes['seasonYear'] ?? date('Y');
 $header = $attributes['header'];
@@ -13,9 +16,7 @@ $linkText = $attributes['linkText'];
 $noEntriesFoundText = $attributes['noEntriesFoundText'];
 
 // find all date entries for the current season
-$allDates = get_field('rundgang_termin') ?? [];
-
-
+$allDates = get_field(FSRG_RUNDGANG_TERMIN_GROUP_FIELD) ?? [];
 
 // prepare the found date entries for the list output
 // each entry has two fields, datum_und_zeit and ticket_link
@@ -26,7 +27,7 @@ $dateList = [];
 if (!empty($allDates)) {
 	foreach ($allDates as $event) {
 		// convert the date string to a DateTime object
-		$eventDate = new DateTime($event["datum_und_zeit"]);
+		$eventDate = new DateTime($event[FSRG_RUNDGANG_TERMIN_DATE_AND_TIME_FIELD]);
 		// if the events date is not in the current season, skip it
 		if ($eventDate->format('Y') !== $seasonYear) {
 			continue;
@@ -59,7 +60,7 @@ if (!empty($allDates)) {
 
 		$dateString = $eventDate->format('d.m.y H:i');
 		$dateString = "$shortDay " . $dateString . " Uhr";
-		$dateList[$dateString] = $event["ticket_link"];
+		$dateList[$dateString] = $event[FSRG_RUNDGANG_TERMIN_TICKET_LINK_FIELD];
 	}
 
 }
@@ -68,19 +69,16 @@ if (empty($dateList)) {
 }
 ?>
 
+<?php echo do_blocks('<!-- wp:paragraph {"metadata":{"name":"Termine"}} -->'); ?>
 <p class="fsrg-block-list-tour-dates-of-season-header">
 	<strong>
 		<?php echo esc_html_e("$header $seasonYear"); ?>
 	</strong>
 </p>
+<?php echo do_blocks('<!-- /wp:paragraph -->'); ?>
+<?php echo do_blocks(FSRG_BLOCK_SPACER_ALLE_10_REF); ?>
 
-<?php // REQUIRES FLEXIBLE SPACER BLOCK PLUGIN ?>
-<div aria-hidden="true" class="wp-block-fsb-flexible-spacer fsb-flexible-spacer">
-	<div class="fsb-flexible-spacer__device fsb-flexible-spacer__device--lg" style="height:10px"></div>
-	<div class="fsb-flexible-spacer__device fsb-flexible-spacer__device--md" style="height:10px"></div>
-	<div class="fsb-flexible-spacer__device fsb-flexible-spacer__device--sm" style="height:10px"></div>
-</div>
-
+<?php echo do_blocks('<!-- wp:list {"metadata":{"name":"Termine"}} -->'); ?>
 <ul class="fsrg-block-list-tour-dates-of-season-list">
 	<?php if ($noDatesFound) { ?>
 		<li class="fsrg-block-list-tour-dates-of-season-entry">
@@ -88,11 +86,14 @@ if (empty($dateList)) {
 		</li>
 	<?php } ?>
 	<?php foreach ($dateList as $date => $link) { ?>
+		<?php echo do_blocks('<!-- wp:list-item -->'); ?>
 		<li class="fsrg-block-list-tour-dates-of-season-entry">
 			<?php echo esc_html_e($date); ?>:
 			<a href="<?php echo esc_url($link); ?>" target="_blank" rel="noreferrer noopener">
 				<?php echo esc_html_e($linkText); ?>
 			</a>
 		</li>
+		<?php echo do_blocks('<!-- /wp:list-item -->'); ?>
 	<?php } ?>
 </ul>
+<?php echo do_blocks('<!-- /wp:list -->'); ?>
