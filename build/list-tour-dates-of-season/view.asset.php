1970454b43c967d5bd15b0d31f64dbc0c9132643
Sebastian Hutter <me@sebastian-hutter.dev>
2024-11-24 16:15:15 +0100

    remove files not to be committed

--- build/list-tour-dates-of-season/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'f1af40bec01573152abd');
--- build/original-block/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1037e11934a455ea8faf');
